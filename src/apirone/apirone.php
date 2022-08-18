38d7b3879303c798c93c1e262e8afb6e1b4804ab
Aiden <84194083+mightybytesuk@users.noreply.github.com>
2022-08-18 07:46:32 +0100

    Initial Commit

---
+++ src/apirone/apirone.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Apirone;
+
+class Apirone {
+
+    function sendPostRequest($endpoint, $data)
+    {
+        $url = 'https://apirone.com/api/v2/'.$endpoint;
+ 
+        $curl = curl_init();
+         
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_POST, TRUE);
+        
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+         
+        $data = curl_exec($curl);
+         
+        curl_close($curl);
+
+        return $data;
+    }
+
+    public function sendGetRequest($endpoint)
+    {
+        $url = 'https://apirone.com/api/v2/'.$endpoint;
+ 
+        $curl = curl_init();
+         
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        
+        $data = curl_exec($curl);
+         
+        curl_close($curl);
+
+        return $data; 
+    }
+
+    public function account()
+    {
+        $this->sendPostRequest('accounts', '');
+    }
+
+    public function checkPayment($account, $currency, $address)
+    {
+        $endpoint = 'accounts/'.$account.'/balance?currency='.$currency.'&addresses='.$address;
+        $this->sendGetRequest($endpoint);
+        
+    }
+
+    public function generateAddress($account, $currency, $callback, $id)
+    {
+        $endpoint = 'accounts/'.$account.'/addresses';
+
+        $data = array(
+            '"currency": "'.$currency.'",
+        "callback": {
+            "method": "POST",
+            "url": "'.$callback.'",
+            "data": {
+                "id": "'.$id.'"'
+            );
+        $this->sendPostRequest($endpoint, $data);
+    }
+}
